5c4afe0d9dad0a9b9baefefde7df89305411895b
AndersSi <AndersSi@147.200.242.145>
2014-07-30 16:55:16 +1000

    Added images to output PDF

--- JMangle/src/PDFGenerator.java
+++ JMangle/src/PDFGenerator.java
@@ -7,21 +7,43 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Image;
 
 public class PDFGenerator 
 {
-	ArrayList<File> images;
+	ArrayList<Image> images;
 	Document pdfDocument;
 	String outPutFilename = "PDFOutput\\default.pdf";
+	String outputDirectory = ".";
 	
 	public PDFGenerator()
 	{
-		images = new ArrayList<File>();		
+		images = new ArrayList<Image>();		
+	}
+	
+	public void setOutputDirectory(String outputDirectory)
+	{
+		this.outputDirectory = outputDirectory;
+	}
+	
+	public void setOutputFilename(String outputFilename)
+	{
+		this.outPutFilename = outputFilename;
 	}
 	
 	public void addImage(File image)
 	{
-		images.add(image);
+		
+		try
+		{
+			Image tmpImage = Image.getInstance(image.getAbsolutePath());
+			images.add(tmpImage);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+        
 	}
 	
 	public void generatePDF()
@@ -31,9 +53,13 @@ public class PDFGenerator
         	pdfDocument = new Document();
             PdfWriter.getInstance(pdfDocument, new FileOutputStream(outPutFilename));
             pdfDocument.open();
-            if(images.size()>2)
+            if(images.size()>1)
             {
-            	//add image to PDF
+            	for(int i=0;i<images.size();i++)
+            	{
+            		pdfDocument.add(images.get(i));
+            		//TODO Add page break
+            	}
             }
             else
             {
